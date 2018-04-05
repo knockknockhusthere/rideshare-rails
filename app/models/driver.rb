1e733e269c5e84ab488f77f32e9c2d971e6402dd
Jamila <J.cornick21@gmail.com>
2018-04-05 15:05:52 -0700

    business logic for driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,17 @@
 class Driver < ApplicationRecord
   has_many :trips
+
+  def total_earnings
+
+    total = 0
+
+    my_rides = self.trips
+    my_rides.each do |ride|
+      raw_cost = ride.cost - 1.65
+      sub_total = raw_cost * 0.8
+
+      total += sub_total
+    end
+    return total/100
+  end
 end
