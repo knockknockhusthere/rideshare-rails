0f7ac9d189f0b1d0320f3a842ddbf1e6a2eb582e
knockknockhusthere <angela.huxia@gmail.com>
2018-04-05 16:27:44 -0700

    made views pages similar

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -6,12 +6,12 @@ class Driver < ApplicationRecord
 
     my_rides = self.trips
     my_rides.each do |ride|
-      raw_cost = ride.cost - 1.65
+      raw_cost = ride.cost - 165
       sub_total = raw_cost * 0.8
 
       total += sub_total
     end
-    return total/100
+    return total / 100
   end
 
   def avg_rating
