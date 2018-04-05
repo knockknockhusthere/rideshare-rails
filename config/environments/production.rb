d7e975c08c5feed2c0580fa707c768e5dcdd37ba
knockknockhusthere <angela.huxia@gmail.com>
2018-04-05 14:22:31 -0700

    added production code for images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,9 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+
+  config.serve_static_assets = true
+  config.assets.compile = true
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
