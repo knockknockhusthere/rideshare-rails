80c7069715b7e06b892b28727dfbd428a2de1ac5
knockknockhusthere <angela.huxia@gmail.com>
2018-04-05 14:52:23 -0700

    added business logic for passenger

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,15 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+  def trips_cost
+
+    my_trips = self.trips
+    total = 0
+
+    my_trips.each do |trip|
+      total += trip.cost
+    end
+
+    return total / 100
+  end
 end
