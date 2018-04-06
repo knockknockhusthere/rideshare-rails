fb3e2bb67d03b31167da256498ee60e871371e62
Jamila <J.cornick21@gmail.com>
2018-04-06 14:30:13 -0700

    added delete link to trips show

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,10 +2,4 @@ class Trip < ApplicationRecord
   belongs_to :passenger
   belongs_to :driver
 
-  def rating
-    my_rating = self.where(rating: nil)
-    my_rating.rating == range(1...5)
-
-  end
-
 end
